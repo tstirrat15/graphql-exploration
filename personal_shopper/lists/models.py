ff73036ff5fa58a74937b0abc4bb77e550d37494
Tanner Stirrat <tstirrat@gmail.com>
2019-07-21 15:58:35 -0600

    Add models and views

--- personal_shopper/lists/models.py
+++ personal_shopper/lists/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    notes = models.TextField()
+    address = models.TextField()
+
+
+class Source(models.Model):
+    name = models.CharField(max_length=100)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    weight = models.FloatField()
+    item_source = models.ForeignKey(Source, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
--- personal_shopper/lists/views.py
+++ personal_shopper/lists/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Client, Source, Item
+from .serializers import ClientSerializer, SourceSerializer, ItemSerializer
 
-# Create your views here.
+
+class ClientViewset(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+
+class SourceViewset(viewsets.ModelViewSet):
+    queryset = Source.objects.all()
+    serializer_class = SourceSerializer
+
+
+class ItemViewset(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
