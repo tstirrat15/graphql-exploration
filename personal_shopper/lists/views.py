ed7ee1ba95cabc2d412bfb6dc16da2ed4d8ab5c8
Tanner Stirrat <tstirrat@gmail.com>
2019-07-22 16:28:37 -0600

    Add a detail view for items

--- personal_shopper/lists/views.py
+++ personal_shopper/lists/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from .models import Client, Source, Item
 from .serializers import ClientSerializer, SourceSerializer, ItemSerializer
 
@@ -6,6 +9,12 @@ from .serializers import ClientSerializer, SourceSerializer, ItemSerializer
 class ClientViewset(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+
+    @action(detail=True)
+    def items(self, request, pk):
+        items = Item.objects.filter(client__id=pk)
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)
 
 
 class SourceViewset(viewsets.ModelViewSet):
